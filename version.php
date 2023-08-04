80dbd283af843b89609e54d7fdb8a90e030f3305
Punja Solanki <punjasolanki197@gmail.com>
2023-08-04 12:28:06 +0530

    version change

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_eflok';
 $plugin->release = '0.1.0';
-$plugin->version = 2023071000;
+$plugin->version = 2023080400;
 $plugin->requires = 2019052000; //2022112800;
 $plugin->maturity = MATURITY_STABLE;
